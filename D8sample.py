12b817ab8d4d5160b28bf301f361ad0f93ef994f
jerry tabon <ryloxlox@gmail.com>
2023-01-21 12:43:50 +0800

    first commit

--- D8sample.py
+++
@@ -1,8 +0,0 @@
-
-#def greet_with_name(name, location):
- #  print(f"hello {name}")
-  # print(f"Where do you live {location}")
-   #print("its nice to meet you")
-
-#greet_with_name(location="japan", name="jay")
-
